86a07243fb6b6c18bd886354a9aa549c28d479cd
mysusu <534406210@qq.com>
2014-09-13 14:15:57 +0800

    finish v1

--- pos_v1_3rdlib/src/main.js
+++ pos_v1_3rdlib/src/main.js
@@ -1,1 +1,100 @@
 //TODO: Please write code in this file.
+//TODO: Please write code in this file.
+function printInventory(inputs) {
+    var allItem   = loadAllItems();
+    var promot    = loadPromotions()[0];
+    var Itemcount = [];
+    var promotCount= [];
+    var goodsPrint = [];//打印买的商品
+    var freePrint = [];//打印赠送商品
+    var totalMoney = 0;//总价
+    var cutMoney = 0;//节省
+
+
+    for(var a=0;a<allItem.length;a++){
+        for(var b=0;b<inputs.length;b++){
+            if(allItem[a].barcode == inputs[b].substring(0,10)){
+
+                if(!Itemcount.length){
+                    var aItem = allItem[a];
+                    if(inputs[b].length > 10){
+                        aItem.count=Number(inputs[b].substring(11,inputs[b].length));
+                    } else {
+                        aItem.count = 1;
+                    }
+                    Itemcount.push(aItem);console.log(Itemcount)
+                } else {
+                    for(var c= 0, length=Itemcount.length;c<length;c++){
+                        if (Itemcount[c].barcode == inputs[b].substring(0,10)){
+                            if(inputs[b].length > 10){
+                                Itemcount[c].count += Number(inputs[b].substring(11,inputs[b].length));
+                            } else {
+                                Itemcount[c].count++;
+                            }
+                            break;
+
+                        }
+                        if(Itemcount[c].barcode != inputs[b].substring(0,10) && c == Itemcount.length - 1){
+                            var bItem=allItem[a];
+                            if(inputs[b].length > 10){
+                                bItem.count=Number(inputs[b].substring(11,inputs[b].length));
+                            } else {
+                                bItem.count = 1;
+                            }
+                            Itemcount.push(bItem);
+                        }
+                    }
+
+                }
+
+            }
+        }
+
+    }
+
+    for(var n = 0; n < Itemcount.length; n++){
+        for(var k = 0; k < promot.barcodes.length;k++) {
+            if(Itemcount[n].barcode == promot.barcodes[k] && Itemcount[n].count>=2){
+                Itemcount[n].subTotalm = (Itemcount[n].count - 1) * Itemcount[n].price;
+                var promotCounts= {name: Itemcount[n].name,price:Itemcount[n].price,unit:Itemcount[n].unit, count: 1};
+                promotCount.push(promotCounts);
+                break;
+            }
+            if(promot.barcodes[k] != Itemcount[n].barcode && k == promot.barcodes.length - 1){
+                Itemcount[n].subTotalm = Itemcount[n].count * Itemcount[n].price;
+            }
+        }
+    }
+
+
+    for(var i = 0; i < Itemcount.length; i++) {
+        goodsPrint[i] = "名称：" + Itemcount[i].name + "，数量：" + Itemcount[i].count + Itemcount[i].unit + "，单价："
+            + Itemcount[i].price.toFixed(2) + "(元)，小计：" + Itemcount[i].subTotalm.toFixed(2)
+            + "(元)\n";
+        totalMoney += Itemcount[i].subTotalm;
+    }
+    for(var j = 0;j <  promotCount.length;j++){
+        freePrint[j]="名称：" + promotCount[j].name + "，数量：" + promotCount[j].count + promotCount[j].unit + "\n";
+        cutMoney += promotCount[j].price;
+    }
+
+    dateDigitToString = function (num) {
+        return num < 10 ? '0' + num : num;
+    };
+    var formattedDateStrings=0;
+
+    var text =
+        '***<没钱赚商店>购物清单***\n' +
+        //   '打印时间：' + currentDate(formattedDateStrings) + '\n' +
+        '----------------------\n' +
+        goodsPrint.join("") +
+        '----------------------\n' +
+        '挥泪赠送商品：\n' +
+        freePrint.join("") +
+        '----------------------\n' +
+        '总计：' + totalMoney.toFixed(2) + '(元)\n' +
+        '节省：' + cutMoney.toFixed(2) + '(元)\n' +
+        '**********************';
+    console.log(text);
+
+}
