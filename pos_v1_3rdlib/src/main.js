34b1cd77a734dba55e2a0562d579ba53383cd465
mysusu <534406210@qq.com>
2014-09-14 14:05:20 +0800

    part of change with underscore

--- pos_v1_3rdlib/src/main.js
+++ pos_v1_3rdlib/src/main.js
@@ -1,86 +1,74 @@
 //TODO: Please write code in this file.
 function printInventory(inputs) {
-    var allItem   = loadAllItems();
-    var promot    = loadPromotions()[0];
+    var allItem = loadAllItems();
+    var promot = loadPromotions()[0];
     var Itemcount = [];
-    var promotCount= [];
+    var promotCount = [];
     var goodsPrint = [];
     var freePrint = [];
     var totalMoney = 0;
     var cutMoney = 0;
 
-
-    for(var a=0;a<allItem.length;a++){
-        for(var b=0;b<inputs.length;b++){
-            if(allItem[a].barcode == inputs[b].substring(0,10)){
-
-                if(!Itemcount.length){
-                    var aItem = allItem[a];
-                    if(inputs[b].length > 10){
-                        aItem.count=Number(inputs[b].substring(11,inputs[b].length));
+    _.map(allItem, function (allI) {
+        _.map(inputs, function (input) {
+            if (allI.barcode == input.substring(0, 10)) {
+                if (!Itemcount.length) {
+                    var aItem = allI;
+                    if (input.length > 10) {
+                        aItem.count = Number(input.substring(11, input.length));
                     } else {
                         aItem.count = 1;
                     }
                     Itemcount.push(aItem);
                 } else {
-                    for(var c= 0, length=Itemcount.length;c<length;c++){
-                        if (Itemcount[c].barcode == inputs[b].substring(0,10)){
-                            if(inputs[b].length > 10){
-                                Itemcount[c].count += Number(inputs[b].substring(11,inputs[b].length));
+
+
+                    for (var c = 0, length = Itemcount.length; c < length; c++) {
+                        if (Itemcount[c].barcode == input.substring(0, 10)) {
+                            if (input.length > 10) {
+                                Itemcount[c].count += Number(input.substring(11, input.length));
                             } else {
                                 Itemcount[c].count++;
                             }
+                        //  Itemcount[c] = input.length > 10 ? Number(input.substring(11, input.length))+Itemcount[]
                             break;
 
                         }
-                        if(Itemcount[c].barcode != inputs[b].substring(0,10) && c == Itemcount.length - 1){
-                            var bItem=allItem[a];
-                            if(inputs[b].length > 10){
-                                bItem.count=Number(inputs[b].substring(11,inputs[b].length));
-                            } else {
-                                bItem.count = 1;
-                            }
+                        if (Itemcount[c].barcode != input.substring(0, 10) && c == Itemcount.length - 1) {
+                            var bItem = allI;
+                            bItem.count = input.length > 10 ? Number(input.substring(11, input.length)) : 1;
                             Itemcount.push(bItem);
                         }
                     }
-
                 }
-
             }
-        }
-
-    }
-
-//    for(var n = 0; n < Itemcount.length; n++){
-//
-//    }
-    _.map(Itemcount,function(n){
-       // _.map(promot.barcodes,function(pro_barcode){
-
-        //})
-        for(var k = 0; k < promot.barcodes.length;k++) {
-            if(n.barcode == promot.barcodes[k]&& n.count>=3 ){
-                    n.subTotalm = (n.count - parseInt(n.count / 3)) * n.price;
-                    var promotCounts= {name: n.name,price:n.price,unit:n.unit, count: 1};
-                    promotCount.push(promotCounts);
-                    break;
-            }
-            if(promot.barcodes[k] != n.barcode && k == promot.barcodes.length - 1){
-                n.subTotalm = n.count * n.price;
-            }
-        }
+        });
     });
 
-    for(var i = 0; i < Itemcount.length; i++) {
-        goodsPrint[i] = "名称：" + Itemcount[i].name + "，数量：" + Itemcount[i].count + Itemcount[i].unit + "，单价："
-            + Itemcount[i].price.toFixed(2) + "(元)，小计：" + Itemcount[i].subTotalm.toFixed(2)
-            + "(元)\n";
-        totalMoney += Itemcount[i].subTotalm;
-    }
-    for(var j = 0;j <  promotCount.length;j++){
-        freePrint[j]="名称：" + promotCount[j].name + "，数量：" + promotCount[j].count + promotCount[j].unit + "\n";
-        cutMoney += promotCount[j].price;
-    }
+    _.map(Itemcount, function (n) {
+        var even = _.find(promot.barcodes, function (promot_bar) {
+            return promot_bar == n.barcode && n.count >= 3
+        });
+        if (even) {
+            n.subTotalm = (n.count - parseInt(n.count / 3)) * n.price;
+            var promotCounts = {name: n.name, price: n.price, unit: n.unit, count: 1};
+            promotCount.push(promotCounts);
+        }
+        else {
+            n.subTotalm = n.count * n.price;
+        }
+
+    });
+    _.map(Itemcount, function (itemCount) {
+        goodsPrint.push("名称：" + itemCount.name + "，数量：" + itemCount.count + itemCount.unit + "，单价："
+            + itemCount.price.toFixed(2) + "(元)，小计：" + itemCount.subTotalm.toFixed(2)
+            + "(元)\n");
+        totalMoney += itemCount.subTotalm;
+    });
+    _.map(promotCount, function (proC) {
+        freePrint.push("名称：" + proC.name + "，数量：" + proC.count + proC.unit + "\n");
+        cutMoney += proC.price;
+    });
     var text =
         '***<没钱赚商店>购物清单***\n' +
 
