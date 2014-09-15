1555b8565fc4e9a17bfe6bff88829ae14f623c5a
mysusu <534406210@qq.com>
2014-09-15 14:11:11 +0800

    function改在一个文件

--- pos_v2/src/main.js
+++ pos_v2/src/main.js
@@ -1,9 +1,3 @@
-
-//function get_promot_in_Itemcount(promot,n){
-//    return _.find(promot.barcodes, function (promot_bar) {
-//        return promot_bar == n.barcode && n.count >= 3
-//    });
-//}
 //TODO: Please write code in this file.
 function printInventory(inputs) {
     var allItem = loadAllItems();
@@ -17,33 +11,30 @@ function printInventory(inputs) {
 
 
     _.map(allItem, function (allI) {
-        _.map(inputs,function(input){
-            if (allI.barcode == input.substring(0, 10)) {
-                if (!Itemcount.length) {
-                    var aItem = allI;
-                    aItem.count = input.length > 10 ? Number(input.substring(11, input.length)) : 1;
-                    Itemcount.push(aItem);
-                } else {
+        var all_input_equal = _.filter(inputs, function (input) {
+            return allI.barcode == input.substring(0, 10);
+        });
+        if (all_input_equal) {
+            if (!Itemcount.length) {
+                Itemcount.push(len(allI, input));
+            }
+            if (Itemcount.length) {
+                var item_even = item_count_s(Itemcount, input);
+                if (item_even) {
+                    item_count_s(Itemcount, input).count = input.length > 10 ? (Number(input.substring(11, input.length)) + item_even.count) : (item_even.count + 1);
+                }
 
-                    for (var c = 0, length = Itemcount.length; c < length; c++) {
-                        if (Itemcount[c].barcode == input.substring(0, 10)) {
-                            Itemcount[c].count = input.length > 10 ?
-                                (Number(input.substring(11, input.length)) + Itemcount[c].count) : (Itemcount[c].count + 1);
-                            break;
-                        }
-                        if (Itemcount[c].barcode != input.substring(0, 10) && c == Itemcount.length - 1) {
-                            var bItem = allI;
-                            bItem.count = input.length > 10 ?
-                                Number(input.substring(11, input.length)) : 1;
-                            Itemcount.push(bItem);
-                        }
-                    }
+                if (!item_even) {
+                    Itemcount.push(len(allI, input));
                 }
             }
-        });
+        }
+
     });
+
+
     _.map(Itemcount, function (n) {
-        var even = get_promot_in_Itemcount(promot,n);
+        var even = get_promot_in_Itemcount(promot, n);
         if (even) {
             n.subTotalm = (n.count - parseInt(n.count / 3)) * n.price;
             var promotCounts = {name: n.name, price: n.price, unit: n.unit, count: 1};
@@ -64,7 +55,7 @@ function printInventory(inputs) {
         cutMoney += proC.price;
     });
 
-    var text =  printText(goodsPrint,freePrint,totalMoney,cutMoney);
+    var text = printText(goodsPrint, freePrint, totalMoney, cutMoney);
     console.log(text);
 
 }
--- pos_v2/src/model/get_pro.js
+++ pos_v2/src/model/get_pro.js
@@ -1,8 +1,54 @@
 /**
  * Created by susu on 14-9-15.
  */
-function get_promot_in_Itemcount(pro,n){
+function get_promot_in_Itemcount(pro, n) {
     return _.find(pro.barcodes, function (promot_bar) {
         return promot_bar == n.barcode && n.count >= 3
     });
+}
+
+function len(allI, input) {
+    var bItem = allI;
+    bItem.count =
+            input.length > 10 ?
+        Number(input.substring(11, input.length)) : 1;
+    return bItem;
+}
+
+function item_count_s(Itemcount, input) {
+    return _.find(Itemcount, function (itemCount) {
+        return itemCount.barcode == input.substring(0, 10)
+    });
+}
+
+function printText(goods,free,total,cut) {
+    formattedDateStr = current_time();
+    var texts =
+        '***<没钱赚商店>购物清单***\n' +
+        '打印时间：' + formattedDateStr + '\n' +
+        '----------------------\n' +
+        goods.join("") +
+        '----------------------\n' +
+        '挥泪赠送商品：\n' +
+        free.join("") +
+        '----------------------\n' +
+        '总计：' + total.toFixed(2) + '(元)\n' +
+        '节省：' + cut.toFixed(2) + '(元)\n' +
+        '**********************';
+    return texts;
+}
+
+function current_time() {
+    dateDigitToString = function (num) {
+        return num < 10 ? '0' + num : num;
+    };
+    var currentDate = new Date(),
+        year = dateDigitToString(currentDate.getFullYear()),
+        month = dateDigitToString(currentDate.getMonth() + 1),
+        date = dateDigitToString(currentDate.getDate()),
+        hour = dateDigitToString(currentDate.getHours()),
+        minute = dateDigitToString(currentDate.getMinutes()),
+        second = dateDigitToString(currentDate.getSeconds()),
+        formattedDateString = year + '年' + month + '月' + date + '日 ' + hour + ':' + minute + ':' + second;
+    return formattedDateString;
 }
